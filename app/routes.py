591fd6192afe9a8a3933f6840fbce008d28edd59
Citlalli I Zarate <37793116+MiffyBruna@users.noreply.github.com>
2022-10-26 15:27:18 -0700

    Create hello-world routes and blueprint to test

---
+++ app/routes.py
@@ -0,0 +1,11 @@
+from crypt import methods
+from flask import Blueprint
+
+hello_world_bp = Blueprint
+("hello_world", __name__)
+
+hello_world_bp = Blueprint("hello_world_bp", __name__)
+
+@hello_world_bp.route("/hello-world", methods=["GET"])
+def say_hello_world():
+    return "Hello, World 🤖!"
