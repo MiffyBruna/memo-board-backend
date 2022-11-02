15dc69df3a45a0a23465a9607ca6a54d144ab44d
Citlalli I Zarate <37793116+MiffyBruna@users.noreply.github.com>
2022-11-01 17:49:05 -0700

    Delete hello world route and define response status codes

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,7 @@
-from crypt import methods
-from email import message
 from flask import Blueprint
 from app.models.board import Board
 from app.models.memo import Memo
-from flask import Blueprint, jsonify, abort, make_response, request
+from flask import Blueprint, jsonify, make_response, request
 from app import db
 
 
@@ -11,13 +9,8 @@ from app import db
 
 hello_world_bp = Blueprint("hello_world_bp", __name__)
 board_bp = Blueprint("board_bp", __name__, url_prefix="/boards")
-memo_bp = Blueprint("memo_bp", __name__, url_prefix="/Memo")
+memo_bp = Blueprint("memo_bp", __name__, url_prefix="/memos")
 
-
-####--------------- HELLO WORLD ---------------####
-@hello_world_bp.route("/hello-world", methods=["GET"])
-def say_hello_world():
-    return "Hello, World 🤖!"
 
 ####--------------- BOARD ROUTES ---------------####
 
@@ -37,24 +30,23 @@ def get_all_boards():
 @board_bp.route("/<board_id>", methods=["GET"])
 def get_board(board_id):
     board= Board.query.get(board_id)
-    
+
     if not board:
-        return {"message": f"memo {board_id} was not found"}, 404
+        return make_response({"message": f"Board {board_id} was not found", "status": 404})
 
     response_body = {
         "board": (board.board_json())
     }
-    return jsonify(response_body.board_json()), 200
+    return jsonify(response_body), 200
 
-@board_bp.route("/<board_id>/cards")
+@board_bp.route("/<board_id>/memos")
 def get_all_cards_from_board(board_id):
     board = Board.query.get(board_id)
     if board is None:
-        return make_response("Board does not exist", 404)
+        return make_response({"message": f"Board {board_id} was not found", "status": 404})
     
     return jsonify(board.all_memos_json()), 200
 
-#POST TODO: POST CARD INSIDE BOARD
 @board_bp.route("", methods=["POST"])
 def create_board():
     request_body = request.get_json()
@@ -62,7 +54,7 @@ def create_board():
     
     for item in items:
         if item not in request_body:
-            return make_response({"details": f"Request body must include {item}"})
+            return make_response({"details": f"Request body must include {item}","status": 222})
     
     new_board = Board(title=request_body["title"],
                     owner=request_body["owner"])
@@ -71,13 +63,14 @@ def create_board():
     db.session.commit()
 
     return jsonify(new_board.board_json()), 201
-@board_bp.route("/<board_id>/memos")
+
+@board_bp.route("/<board_id>/memos", methods =["POST"])
 def post_card_to_given_board(board_id):
     request_body = request.get_json()
     board= Board.query.get(board_id)
 
     if not board:
-        return make_response({"message": f"Board {board_id} does not exist"})
+        return make_response({"message": f"Board {board_id} does not exist", "status": 404})
 
     new_memo = Memo(message=request_body["message"],board_id= board_id)
     db.session.add(new_memo)
@@ -92,15 +85,15 @@ def delete_board(board_id):
     board= Board.query.get(board_id)
 
     if not board:
-        return make_response({"message": f"Board {board_id} does not exist"})
+        return make_response({"message": f"Board {board_id} does not exist","status": 404})
     db.session.delete(board)
     db.session.commit()
 
-    return make_response({"message: Board deleted successfully"}),200
+    return make_response({"message": f"Board deleted successfully", "status": 204})
 
 
 ####--------------- MEMO ROUTES ---------------####
-@memo_bp.route("", methods=["DELETE"])
+@memo_bp.route("/<memo_id>", methods=["DELETE"])
 
 def delete_memo(memo_id):
     memo = Memo.query.get(memo_id)
@@ -108,7 +101,7 @@ def delete_memo(memo_id):
     db.session.commit()
 
     if not memo:
-        return {"message": f"memo {memo_id} was not found"}, 404
+        return make_response({{"message": f"memo {memo_id} was not found"}: 404})
 
     return {
             "details":\
@@ -122,7 +115,7 @@ def add_likes(memo_id):
     memo.likes_count +=1
     db.session.commit()
 
-    return make_response(memo.memo_json())
+    return make_response(memo.memo_json()), 200
 
 
 
